bd495ba208ceed05ddbe75667d8594ab67f4960a
MD.JIHAD HOSSAIN <43439618+mdjihad560@users.noreply.github.com>
2024-01-15 20:33:47 +0600

    create zubuz html file

---
+++ assets/js/app.js
@@ -0,0 +1,364 @@
+"use strict";
+
+(function ($) {
+  "use strict";
+
+  /*------------------------------------------------------------------
+  [Table of contents]
+  zubuz PRELOADER JS INIT
+  zubuz SKILLBAR JS INIT
+  zubuz MENU SIDEBAR JS INIT
+  zubuz COUNTER JS INIT
+  zubuz IMAGE ROTATE JS INIT
+  zubuz AUTO SLIDER JS INIT
+  zubuz PROJECT SLIDER JS INIT
+  zubuz PROJECT SLIDER2 JS INIT
+  zubuz MAGNIFIC POPUP JS INIT
+  zubuz IMAGE SWIPE HOVER JS INIT
+  zubuz PRICING TABLE JS INIT
+  zubuz MAP JS
+  zubuz wow js
+  
+  -------------------------------------------------------------------*/
+
+  /*--------------------------------------------------------------
+  CUSTOM PRE DEFINE FUNCTION
+  ------------------------------------------------------------*/
+  /* is_exist() */
+  jQuery.fn.is_exist = function () {
+    return this.length;
+  };
+  $(function () {
+    /*--------------------------------------------------------------
+    zubuz PRELOADER JS INIT
+    --------------------------------------------------------------*/
+
+    $(".zubuz-preloader-wrap").fadeOut(500);
+
+    /*--------------------------------------------------------------
+    zubuz SKILLBAR JS INIT
+    ------------------------------------------------------------*/
+    var skillbar = $('.zubuz-skillbar');
+    if (skillbar.is_exist()) {
+      skillbar.skillBars({
+        from: 0,
+        speed: 4000,
+        interval: 100,
+        decimals: 0
+      });
+    }
+
+    /*--------------------------------------------------------------
+    zubuz STICKY MENU JS INIT
+    --------------------------------------------------------------*/
+    $(window).on('scroll', function () {
+      if ($(window).scrollTop() > 50) {
+        $('#sticky-menu').addClass('sticky-menu');
+      } else {
+        $('#sticky-menu').removeClass('sticky-menu');
+      }
+    });
+
+    /*--------------------------------------------------------------
+    zubuz MENU SIDEBAR JS INIT
+    --------------------------------------------------------------*/
+    $(".barger-menu").on("click", function (e) {
+      $(".zubuz-sidemenu-column, .offcanvas-overlay").addClass("active");
+      event.preventDefault(e);
+    });
+    $(".zubuz-sidemenu-close, .offcanvas-overlay").on("click", function () {
+      $(".zubuz-sidemenu-column, .offcanvas-overlay").removeClass("active");
+    });
+
+    /*--------------------------------------------------------------
+    zubuz COUNTER JS INIT
+    --------------------------------------------------------------*/
+    var zubuz_counter = $('#zubuz-counter');
+    if (zubuz_counter.is_exist()) {
+      var a = 0;
+      $(window).scroll(function () {
+        var oTop = $(zubuz_counter).offset().top - window.innerHeight;
+        if (a == 0 && $(window).scrollTop() > oTop) {
+          $('.zubuz-counter').each(function () {
+            var $this = $(this),
+              countTo = $this.attr('data-percentage');
+            $({
+              countNum: $this.text()
+            }).animate({
+              countNum: countTo
+            }, {
+              duration: 4000,
+              easing: 'swing',
+              step: function step() {
+                $this.text(Math.floor(this.countNum));
+              },
+              complete: function complete() {
+                $this.text(this.countNum);
+              }
+            });
+          });
+          a = 1;
+        }
+      });
+    }
+
+    /*--------------------------------------------------------------
+    fugu CLIENT SLIDER
+    --------------------------------------------------------------*/
+    var zubuz_brand_slider = $('.zubuz-brand-slider');
+    if (zubuz_brand_slider.is_exist()) {
+      zubuz_brand_slider.slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: false,
+        autoplay: true,
+        autoplaySpeed: 0,
+        speed: 10000,
+        cssEase: 'linear',
+        pauseOnHover: true,
+        adaptiveHeight: true,
+        responsive: [{
+          breakpoint: 991,
+          settings: {
+            slidesToShow: 3
+          }
+        }, {
+          breakpoint: 767,
+          settings: {
+            slidesToShow: 2
+          }
+        }]
+      });
+    }
+
+    /*--------------------------------------------------------------
+    fugu CLIENT SLIDER
+    --------------------------------------------------------------*/
+    var zubuz_brand_slider = $('.zubuz-brand-slider2');
+    if (zubuz_brand_slider.is_exist()) {
+      zubuz_brand_slider.slick({
+        rtl: true,
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: false,
+        autoplay: true,
+        autoplaySpeed: 0,
+        speed: 10000,
+        cssEase: 'linear',
+        pauseOnHover: true,
+        adaptiveHeight: true,
+        responsive: [{
+          breakpoint: 991,
+          settings: {
+            slidesToShow: 3
+          }
+        }, {
+          breakpoint: 767,
+          settings: {
+            slidesToShow: 2
+          }
+        }]
+      });
+    }
+
+    /*--------------------------------------------------------------
+    zubuz MAGNIFIC POPUP JS INIT
+    ------------------------------------------------------------*/
+    var popup_youtube = $('.video-init');
+    if (popup_youtube.is_exist()) {
+      popup_youtube.magnificPopup({
+        type: 'iframe',
+        mainClass: 'mfp-fade'
+      });
+    }
+
+    /*--------------------------------------------------------------
+    zubuz PRICING TABLE JS INIT
+    ------------------------------------------------------------*/
+    // Table BTN Trigger
+    $("#l5-pricing-btn .toggle-btn").on("click", function (e) {
+      console.log($(e.target).parent().parent().hasClass("monthly-active"));
+      $(e.target).toggleClass("clicked");
+      if ($(e.target).parent().parent().hasClass("monthly-active")) {
+        $(e.target).parent().parent().removeClass("monthly-active").addClass("yearly-active");
+      } else {
+        $(e.target).parent().parent().removeClass("yearly-active").addClass("monthly-active");
+      }
+    });
+    $("[data-pricing-trigger]").on("click", function (e) {
+      $(e.target).addClass("active").siblings().removeClass("active");
+      var target = $(e.target).attr("data-target");
+      console.log($(target).attr("data-value-active") == "monthly");
+      if ($(target).attr("data-value-active") == "monthly") {
+        $(target).attr("data-value-active", "yearly");
+      } else {
+        $(target).attr("data-value-active", "monthly");
+      }
+    });
+  }); /*End document ready*/
+
+  $(window).on("resize", function () {}); // end window resize
+
+  $(window).on("load", function () {
+    var wow = new WOW({
+      mobile: true,
+      // default
+      tablet: true,
+      callback: function callback(box) {
+        if (box.classList.contains('zubuz_animate')) {
+          box.classList.add("zubuz_card_loaded");
+        }
+      }
+    });
+    if ($(window).width() >= 768) {
+      wow.init();
+    }
+  }); // End window LODE
+
+  /*--------------------------------------------------------------
+  zubuz MAP JS
+  ------------------------------------------------------------*/
+  var google_map = $('#map');
+  if (google_map.is_exist()) {
+    var init = function init() {
+      var mapOptions = {
+        zoom: 11,
+        scrollwheel: false,
+        navigationControl: false,
+        mapTypeControl: false,
+        scaleControl: false,
+        draggable: true,
+        disableDefaultUI: true,
+        center: new google.maps.LatLng(40.6700, -73.9400),
+        styles: [{
+          "featureType": "landscape.man_made",
+          "elementType": "geometry",
+          "stylers": [{
+            "color": "#f7f1df"
+          }]
+        }, {
+          "featureType": "landscape.natural",
+          "elementType": "geometry",
+          "stylers": [{
+            "color": "#d0e3b4"
+          }]
+        }, {
+          "featureType": "landscape.natural.terrain",
+          "elementType": "geometry",
+          "stylers": [{
+            "visibility": "off"
+          }]
+        }, {
+          "featureType": "poi",
+          "elementType": "labels",
+          "stylers": [{
+            "visibility": "off"
+          }]
+        }, {
+          "featureType": "poi.business",
+          "elementType": "all",
+          "stylers": [{
+            "visibility": "off"
+          }]
+        }, {
+          "featureType": "poi.medical",
+          "elementType": "geometry",
+          "stylers": [{
+            "color": "#fbd3da"
+          }]
+        }, {
+          "featureType": "poi.park",
+          "elementType": "geometry",
+          "stylers": [{
+            "color": "#bde6ab"
+          }]
+        }, {
+          "featureType": "road",
+          "elementType": "geometry.stroke",
+          "stylers": [{
+            "visibility": "off"
+          }]
+        }, {
+          "featureType": "road",
+          "elementType": "labels",
+          "stylers": [{
+            "visibility": "off"
+          }]
+        }, {
+          "featureType": "road.highway",
+          "elementType": "geometry.fill",
+          "stylers": [{
+            "color": "#ffe15f"
+          }]
+        }, {
+          "featureType": "road.highway",
+          "elementType": "geometry.stroke",
+          "stylers": [{
+            "color": "#efd151"
+          }]
+        }, {
+          "featureType": "road.arterial",
+          "elementType": "geometry.fill",
+          "stylers": [{
+            "color": "#ffffff"
+          }]
+        }, {
+          "featureType": "road.local",
+          "elementType": "geometry.fill",
+          "stylers": [{
+            "color": "black"
+          }]
+        }, {
+          "featureType": "transit.station.airport",
+          "elementType": "geometry.fill",
+          "stylers": [{
+            "color": "#cfb2db"
+          }]
+        }, {
+          "featureType": "water",
+          "elementType": "geometry",
+          "stylers": [{
+            "color": "#a2daf2"
+          }]
+        }]
+      };
+      var mapElement = document.getElementById('map');
+      var map = new google.maps.Map(mapElement, mapOptions);
+      var marker = new google.maps.Marker({
+        position: new google.maps.LatLng(40.6700, -73.9400),
+        map: map,
+        // icon: 'assets/images/all-img/contact/map.png',
+        title: 'fugu'
+      });
+      var contentString = '<div id="content">' + '<div id="tpw">' + '<h3>fugu' + '</div>';
+      var infowindow = new google.maps.InfoWindow({
+        content: contentString,
+        maxWidth: 280
+      });
+      marker.setAnimation(google.maps.Animation.BOUNCE);
+      setTimeout(function () {
+        marker.setAnimation(null);
+      }, 750); //time it takes for one bounce   
+
+      google.maps.event.addListener(marker, 'click', function () {
+        infowindow.open(map, marker);
+      });
+    };
+    google.maps.event.addDomListener(window, 'load', init);
+  }
+
+  //zubuz wow js
+  // var wow = new WOW({
+  //   mobile: false,       // default
+  //   tablet:false
+  // });
+  // wow.init();
+
+  // AOS.init({
+  //   duration: 1200,
+  // })
+})(jQuery);
